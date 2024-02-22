0b773b639289233dcfe301272053f32f8d173efc
Daniel Llumigusin <dgllumigusin@espe.edu.ec>
2024-02-22 16:38:05 -0500

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/espe" element={<ParadaEspe parada={"ESPE"} cooperativa={"Amaguaña"} bus={25} tiempo={("01:20:12")}/>} />
-          <Route path="/sanluis" element={<ParadaEspe parada={"San Luis"}/>} />
+          <Route path="/espe" element={<ParadaEspe parada={"ESPE"} cooperativa={"Amaguaña"} bus={25} tiempo={"17:20:12"}/>} />
+          <Route path="/sanluis" element={<ParadaEspe parada={"San Luis"} cooperativa={"Amaguaña"} bus={25} tiempo={"17:25:12"}/>} />
           <Route path="/triangulo" element={<ParadaEspe parada={"Triangulo"}/>} />
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/Paradas.jsx
+++ frontend/src/pages/Paradas.jsx
@@ -1,39 +1,49 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import Clock from "react-live-clock";
-
+import "../styles/paradas.css";
 const Paradas = ({ parada, bus, cooperativa, tiempo }) => {
   const [diferenciaMinutos, setDiferenciaMinutos] = useState(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
-        const horaActual = moment().tz('America/Guayaquil');
-        const horaLlegada = moment.tz(tiempo, 'HH:mm:ss', 'America/Guayaquil');
-        const diffMinutos = horaLlegada.diff(horaActual, "minutes");        
+      const horaActual = moment().tz("America/Guayaquil");
+      const horaLlegada = moment.tz(tiempo, "HH:mm:ss", "America/Guayaquil");
+      const diffMinutos = horaLlegada.diff(horaActual, "minutes");
       setDiferenciaMinutos(diffMinutos);
-    }, 30000); 
+    }, 1000);
 
     return () => clearInterval(interval);
   }, [tiempo]);
 
   return (
-    <div>
-      <h1>Fecha</h1>      
-      <h1><Clock format={"DD/MM/YYYY"} ticking={true} timezone={"America/Guayaquil"} /></h1>
-      <h2>
-        Hora: 
-        <Clock format={"HH:mm:ss"} ticking={true} timezone={"America/Guayaquil"} />
-      </h2>
-      <h1>Bienvenidos a nuestro sistema de transporte</h1>
-      <h2>Parada {parada}</h2>
-
+    <div className="content-paradas">
+      <div className="titulos-parada">
+        <h1>
+          <Clock
+            format={"DD/MM/YYYY"}
+            ticking={true}
+            timezone={"America/Guayaquil"}
+          />
+        </h1>
+        <h1 className="reloj">
+          <Clock
+            format={"HH:mm:ss"}
+            ticking={true}
+            timezone={"America/Guayaquil"}
+          />
+        </h1>
+        
+        <h2>Bienvenidos a nuestro sistema de transporte</h2>
+        <h1>Parada {parada}</h1>
+      </div>
       <table className="parada-buses">
         <thead>
           <tr>
             <th>Bus</th>
             <th>Cooperativa</th>
             <th>Tiempo Estimado de llegada</th>
-            <th>Aprox.</th>
+            <th>Aprox.{"(min)"}</th>
           </tr>
         </thead>
         <tbody>
@@ -41,8 +51,7 @@ const Paradas = ({ parada, bus, cooperativa, tiempo }) => {
             <td>{bus}</td>
             <td>{cooperativa}</td>
             <td>{tiempo}</td>
-            <td>{diferenciaMinutos !== null ? diferenciaMinutos : "-"}</td>
-            
+            <td>{diferenciaMinutos > 0 ? diferenciaMinutos : "Ya se paso"}</td>
           </tr>
         </tbody>
       </table>
