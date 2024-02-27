99af79c788a0585f7dd1dee1d9df36316dc3fc00
daniela1751 <ddpilataxi@espe.edu.ec>
2024-02-27 09:49:22 -0500

    Cambio
    j

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import "../styles/fondo.css";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Bienvenido a nuestro sistema de información de buses.</h1>
+    <div className="container-home">
+      <div className="Titulos">
+        <h1>Bienvenido a nuestro sistema de información de buses</h1>
+      </div>
+      <button className="boton">
+        <h1>Parada ESPE</h1>
+      </button>
+      <button className="boton">
+        <h1>Parada SAN LUIS</h1>
+      </button>
     </div>
   );
 };
 
 export default Home;
+
